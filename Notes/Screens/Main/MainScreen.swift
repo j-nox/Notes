b754be461995a62d9e17a0e19e2df1de84e641e0
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-13 17:42:51 +0300

    Show default list (#8)

--- Notes/Screens/Main/MainScreen.swift
+++ Notes/Screens/Main/MainScreen.swift
@@ -1,16 +1,14 @@
 import SwiftUI
 
 struct MainScreen: View {
-  var img: String = "https://cs12.pikabu.ru/post_img/2020/01/26/7/og_og_1580037906256995190.jpg"
+  var notes: [Note] = [Note(content: "Note content"), Note(content: "Note two")]
+  
   var body: some View {
-    HStack(alignment:.top) {
-      AsyncImage(url: URL(string: "\(img)")) { image in
-        image.resizable()
-      } placeholder: {
-        Color.red
+    ScrollView {
+      ForEach(notes) { note in
+        Text(note.content)
       }
-      .frame(width: 300, height: 190)
-      .clipShape(RoundedRectangle(cornerRadius: 25))
     }
   }
+  
 }
