4c015d51b03ea9f0e77fb74cae7d25832160a8d2
JNox <32821440+j-nox@users.noreply.github.com>
2022-10-13 17:55:08 +0300

    New note sheet (#10)

--- Notes/Screens/Main/MainScreen.swift
+++ Notes/Screens/Main/MainScreen.swift
@@ -1,13 +1,28 @@
 import SwiftUI
 
 struct MainScreen: View {
+  @State var createSheetStatus: Bool = false
+  
   var notes: [Note] = [Note(content: "Note content"), Note(content: "Note two")]
   
   var body: some View {
-    ScrollView {
-      ForEach(notes) { note in
-        Text(note.content)
+    VStack {
+      Button {
+        createSheetStatus.toggle()
+      } label: {
+        Text("Create")
       }
+      .padding()
+      
+      ScrollView {
+        ForEach(notes) { note in
+          Text(note.content)
+        }
+      }
+    }
+    
+    .sheet(isPresented: $createSheetStatus) {
+      Text("Stub")
     }
   }
   
