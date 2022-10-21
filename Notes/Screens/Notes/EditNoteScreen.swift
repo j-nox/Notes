93888cfb11963d5841469e1a4db3bdb8a01ad54e
JNox <antifry@gmail.com>
2022-10-21 19:43:52 +0300

    Deleting a note

--- Notes/Screens/Notes/EditNoteScreen.swift
+++ Notes/Screens/Notes/EditNoteScreen.swift
@@ -1,21 +1,33 @@
 import SwiftUI
 
 struct EditNoteScreen: View {
+  
   @StateObject var viewModel: EditNoteViewModel = EditNoteViewModel()
+  
   @State var note: Note
+  
   @Binding var notes: [Note]
   
   var body: some View {
     VStack {
-      
       TextField("Content of note", text: $note.content)
       
       Button {
-//      notes[notes.firstIndex { $0.id == note.id }!].content = note.content
+    
         viewModel.note = note
+        
         viewModel.updateNote()
       } label: {
         Text("Save")
+      }
+      
+      Button {
+    
+        viewModel.note = note
+        
+        viewModel.deleteNote()
+      } label: {
+        Text("Delete")
       }
 
     }
